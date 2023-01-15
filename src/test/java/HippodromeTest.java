8b7a2d2147e722423d2b6206f13ce25523480759
Aleksandr Markov <markov.java@gmail.com>
2023-01-15 19:34:21 +0300

    moveTest and refactoring

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +13,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HippodromeTest {
 
-    private final List<Horse> horses = new ArrayList<>();
+    private List<Horse> horses = new ArrayList<>();
+
+    private List<Horse> horsesMock = new ArrayList<>();
 
     @BeforeEach
     void init() {
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 30; i++) {
             horses.add(new Horse(String.valueOf(i), i));
         }
+        for (int i = 0; i < 50; i++) {
+            horsesMock.add(Mockito.mock(Horse.class));
+        }
+    }
+
+    @AfterEach
+    void clear() {
+        horses = null;
+        horsesMock = null;
     }
 
     @ParameterizedTest
@@ -48,4 +61,12 @@ public class HippodromeTest {
         Hippodrome hippodrome = new Hippodrome(horses);
         assertEquals(horses, hippodrome.getHorses());
     }
+
+    @Test
+    void moveTest() {
+        Hippodrome hippodrome = new Hippodrome(horsesMock);
+        hippodrome.move();
+        horsesMock.forEach(mockedHorse -> Mockito.verify(mockedHorse).move());
+    }
+
 }
